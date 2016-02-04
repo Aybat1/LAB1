1b785a5d224a1788be25f546f819ba2d863f2852
Айбат <aibatrakhim97@gmail.com>
2016-02-04 15:29:23 +0600

    1lab-3task student

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public string ID;
+        public double GPA;
+        public Student(string name, string surname, string ID, double GPA)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.ID = ID;
+            this.GPA = GPA;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + ID + " " + GPA;
+        }
+
+
+    }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string a, b, c;
+                double d;
+            Console.WriteLine("How many students you want involve?");
+            int count = int.Parse(Console.ReadLine());
+            Student[] infor= new Student[count];
+            for (int i = 0; i < count; i++)
+            {
+                try { Console.WriteLine("Write the first name");
+                    a = Console.ReadLine();
+                    Console.WriteLine("Write the second name");
+                    b = Console.ReadLine();
+                    Console.WriteLine("Write the your id");
+                    c = Console.ReadLine();
+                    Console.WriteLine("Write the your gpa");
+                    d = double.Parse(Console.ReadLine());
+                    infor[i] = new Student(a, b, c, d);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.ReadKey();
+                }
+
+            }
+            for(int i=0; i<count;i++)
+            {
+                Console.WriteLine(infor[i]);
+            }
+            Console.ReadKey();
+            }
+        }
+    }
