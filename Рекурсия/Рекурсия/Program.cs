eb561b82f1db1c0724944583ad27f6919329f3eb
Айбат <aibatrakhim97@gmail.com>
2016-02-10 13:16:31 +0600

    2lab-1task

---
+++ Рекурсия/Рекурсия/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Рекурсия
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Search(@"C:\Games");
+        }
+        static void Search(string abr)
+        {
+            Stack<string> stack = new Stack<string>();
+            Console.WriteLine(abr + ":" + Directory.GetFiles(abr).Length);
+            stack.Push(abr);
+            while (stack.Count > 0)
+            {
+                string abc = stack.Pop();
+                string[] qwe = Directory.GetDirectories(abc);
+                foreach (string str in qwe)
+                {
+                    Console.WriteLine(str + ":" + Directory.GetFiles(str).Length);
+                    stack.Push(str);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
